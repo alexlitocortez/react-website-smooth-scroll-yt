e3011a644031d405fb7da62119db8018c6d8cba5
Alex Cortez <acim650@gmail.com>
2021-05-14 13:14:47 -0700

    src/components

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Nav, NavbarContainer, Navlogo } from './NavbarElements'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <Navlogo>dolla</Navlogo>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
